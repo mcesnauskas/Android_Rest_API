9c82a1ce8ca44dbedda7c9fcc0d67cfcae7c3b5b
Mindaugas <mcesnauskas@gmail.com>
2024-05-07 11:56:50 +0300

    Get user response by RestAPI

--- app/src/main/java/lt/mindaugas/androidrestapi/MainActivity.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         RemoteRepository remoteRepository = new RemoteRepository();
         remoteRepository.fetchAllUsers();
+        remoteRepository.fetchUserId(1);
     }
 }
--- app/src/main/java/lt/mindaugas/androidrestapi/repository/RemoteRepository.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/repository/RemoteRepository.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.util.HashMap;
 import java.util.Map;
 
+import lt.mindaugas.androidrestapi.entity.UserResponse;
 import lt.mindaugas.androidrestapi.entity.UsersResponse;
 import lt.mindaugas.androidrestapi.network.UserDataService;
 import lt.mindaugas.androidrestapi.network.UserServiceClient;
@@ -39,5 +40,26 @@ public class RemoteRepository {
                     }
                 }
         );
+
     }
+
+    public void fetchUserId(long userId){
+        service.getUser(userId).enqueue(
+                new Callback<UserResponse>() {
+                    @Override
+                    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                        Log.i("tst_rest_api", "onResponse: " + response.body());
+                        Log.i("tst_rest_api", "onResponse: " + response.body().getUser());
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserResponse> call, Throwable t) {
+                        Log.i("tst_rest_api", "Failed to retrieve data" + t.getMessage());
+                        call.cancel();
+                    }
+                }
+        );
+    }
+
+
 }
