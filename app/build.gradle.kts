96f1ae644d4eebecb72cf2166c1a66d017b06165
Mindaugas <mcesnauskas@gmail.com>
2024-05-07 09:43:44 +0300

    Add all dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // retrofit 2
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // recyclerview
+    implementation(libs.recyclerview)
+    implementation(libs.recyclerview.selection)
+
+    // cardview
+    implementation(libs.cardview)
+
+    // glide
+    implementation(libs.glide)
 }
