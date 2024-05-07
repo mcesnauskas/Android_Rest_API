b3bf52994f0060578780d73ea7bba5ab77855da2
Mindaugas <mcesnauskas@gmail.com>
2024-05-07 11:41:38 +0300

    Get users response by RestAPI

--- app/src/main/java/lt/mindaugas/androidrestapi/MainActivity.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/MainActivity.java
@@ -8,11 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import lt.mindaugas.androidrestapi.repository.RemoteRepository;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RemoteRepository remoteRepository = new RemoteRepository();
+        remoteRepository.fetchAllUsers();
     }
 }
---
+++ app/src/main/java/lt/mindaugas/androidrestapi/repository/RemoteRepository.java
@@ -0,0 +1,43 @@
+package lt.mindaugas.androidrestapi.repository;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lt.mindaugas.androidrestapi.entity.UsersResponse;
+import lt.mindaugas.androidrestapi.network.UserDataService;
+import lt.mindaugas.androidrestapi.network.UserServiceClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RemoteRepository {
+    private UserDataService service;
+
+    public RemoteRepository() {
+        this.service = UserServiceClient.getInstance().create(UserDataService.class);
+    }
+
+    public void fetchAllUsers() {
+        Map<String, String> requestParam = new HashMap<>();
+        requestParam.put("page", "1");
+        requestParam.put("per_page", "12");
+
+        service.getAllUsers(requestParam).enqueue(
+                new Callback<UsersResponse>() {
+                    @Override
+                    public void onResponse(Call<UsersResponse> call, Response<UsersResponse> response) {
+                        Log.i("tst_rest_api", "onResponse: " + response.body());
+                        Log.i("tst_rest_api", "onResponse: " + response.body().getUsers());
+                    }
+
+                    @Override
+                    public void onFailure(Call<UsersResponse> call, Throwable t) {
+                        Log.i("tst_rest_api", "Failed to retrieve data" + t.getMessage());
+                        call.cancel();
+                    }
+                }
+        );
+    }
+}
