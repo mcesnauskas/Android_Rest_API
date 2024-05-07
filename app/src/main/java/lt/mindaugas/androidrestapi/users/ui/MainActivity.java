b3b91662e5b3f6da2b0c162fd2f3d94fb7fc5872
Mindaugas <mcesnauskas@gmail.com>
2024-05-07 12:59:34 +0300

    Add ViewBinding and display list of users

--- app/src/main/java/lt/mindaugas/androidrestapi/users/ui/MainActivity.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/users/ui/MainActivity.java
@@ -1,36 +1,51 @@
 package lt.mindaugas.androidrestapi.users.ui;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
-import lt.mindaugas.androidrestapi.R;
-import lt.mindaugas.androidrestapi.repository.RemoteRepository;
+import java.util.ArrayList;
+import java.util.List;
+
+import lt.mindaugas.androidrestapi.databinding.ActivityMainBinding;
+import lt.mindaugas.androidrestapi.entity.User;
 import lt.mindaugas.androidrestapi.users.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel mainViewModel = null;
+    private ActivityMainBinding binding;
+    private ArrayAdapter<User> arrayAdapter;
+    private final List<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         setUpLiveDataObserver();
+
+        setUpListView();
     }
 
     private void setUpLiveDataObserver() {
         mainViewModel.getUsersResponseLiveData().observe(this, usersResponse -> {
             if (usersResponse != null) {
-                Log.i("tst_rest_api", "setUpLiveDataObserver: " + usersResponse);
+                users.addAll(usersResponse.getUsers());
+                arrayAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    private void setUpListView() {
+        arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                users
+        );
+        binding.usersListView.setAdapter(arrayAdapter);
     }
 }
