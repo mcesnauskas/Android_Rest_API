f58862bd26056ae12fdad69fcf1e6e8bc3b08cee
Mindaugas <mcesnauskas@gmail.com>
2024-05-08 10:28:36 +0300

    Set up RecyclerView in MainActivity

--- app/src/main/java/lt/mindaugas/androidrestapi/users/ui/MainActivity.java
+++ app/src/main/java/lt/mindaugas/androidrestapi/users/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package lt.mindaugas.androidrestapi.users.ui;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,7 +16,7 @@ import lt.mindaugas.androidrestapi.users.MainViewModel;
 public class MainActivity extends AppCompatActivity {
     private MainViewModel mainViewModel = null;
     private ActivityMainBinding binding;
-    private ArrayAdapter<User> arrayAdapter;
+    private RecyclerAdapter recyclerAdapter;
     private final List<User> users = new ArrayList<>();
 
     @Override
@@ -34,16 +33,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpRecyclerView() {
         binding.usersRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAdapter = new RecyclerAdapter(this, users);
+        binding.usersRecycleView.setAdapter(recyclerAdapter);
     }
 
     private void setUpLiveDataObserver() {
         mainViewModel.getUsersResponseLiveData().observe(this, usersResponse -> {
             if (usersResponse != null) {
                 users.addAll(usersResponse.getUsers());
-                arrayAdapter.notifyDataSetChanged();
+                recyclerAdapter.addList(users);
             }
         });
     }
-
 
 }
